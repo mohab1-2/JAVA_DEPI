1f114fa717f8158e35e4d7f0335fd7972bc2f3e2
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-07-08 18:55:41 +0300

    Add files via upload

---
+++ Java_DEPI/JavaS2/src/Main.java
@@ -0,0 +1,107 @@
+/// ///////////////////////////FIRST ASSIGNMENT////////////////////////////////////////
+//import java.util.Scanner;
+//
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        Scanner user=new Scanner(System.in);
+//        System.out.println("hello user.");
+//        System.out.println("enter first number ");
+//        int num_1 =user.nextInt();
+//        System.out.println("enter second number ");
+//        int num_2 =user.nextInt();
+//        System.out.println("enter the opertaion you want:");
+//        System.out.println("1. Adition\n" + "2. Subtraction\n" +"3. Multiplication\n" + "4. Division\n");
+//        int x=user.nextInt();
+//        double addition = num_1 + num_2;
+//        double subtraction= num_1 - num_2;
+//        double multiplication= num_1 * num_2;
+//        double devision = num_1 / num_2;
+//
+//        if(x==1)
+//        {
+//            System.out.println(("the addition of " + num_1 + " and " + num_2 + " is: " + addition));
+//        }
+//        if(x==2) {
+//            System.out.println(("the subtration of " + num_1 + " and " + num_2 + " is: " + subtraction));
+//        }
+//        if(x==3) {
+//            System.out.println(("the multiply of " + num_1 + " and " + num_2 + " is: " + multiplication));
+//        }
+//        if(x==4) {
+//            System.out.println(("the divided of " + num_1 + " and " + num_2 + " is: " + devision));
+//        }
+//    }
+//}
+
+
+//////////////////////////////SECOND ASSIGNMENT////////////////////////////////////////
+//import java.util.Scanner;
+//
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        Scanner user=new Scanner(System.in);
+//        System.out.println("write your sentence: ");
+//        String sentence= user.nextLine();
+//        String upper_case=sentence.toUpperCase();
+//        int char_count=sentence.length();
+//        char first_char= sentence.charAt(0);
+//        System.out.println("in uppercase: "+ upper_case);
+//        System.out.println("the length of the sentence is: "+ char_count);
+//        System.out.println("the first characters is: "+ first_char);
+//    }
+//}
+
+
+
+//////////////////////////////third ASSIGNMENT////////////////////////////////////////
+//import java.util.Scanner;
+//
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//     Scanner user=new Scanner(System.in);
+//        System.out.println("enter your age ");
+//        int Age=user.nextInt();
+//        System.out.println("enter your nationality");
+//        String Nationality=user.next();
+//        if(Age>=18 && Nationality.equals("egyptian"))
+//        {
+//            System.out.println("you are Eligible to vote");
+//        }
+//        else
+//        {
+//            System.out.println("you are not Eligible to vote");
+//        }
+//
+//    }
+//}
+
+
+
+
+//////////////////////////////Forth ASSIGNMENT////////////////////////////////////////
+//import java.util.Scanner;
+//
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        Scanner user = new Scanner(System.in);
+//        System.out.println("enter your number: ");
+//        int number= user.nextInt();
+//        if(number % 2 ==0)
+//        {
+//            System.out.println("your number is even");
+//        }
+//        if(number % 2 !=0)
+//        {
+//            System.out.println("your number is odd");
+//        }
+//    }
+//}
+
